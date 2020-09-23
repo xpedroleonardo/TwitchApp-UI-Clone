6532687c9931ee1fce8f357c725d1670d4a22a39
xpedroleonardo <aispedroleonardo@gmail.com>
2020-09-23 19:50:41 -0300

    Adicionando categorias.

--- src/components/CategoryList/data.ts
+++ src/components/CategoryList/data.ts
@@ -4,6 +4,11 @@ import csgo from '../../images/category_csgo.jpg';
 import fortnite from '../../images/category_fortnite.jpg';
 import fallguys from '../../images/category_fallguys.jpg';
 import apex from '../../images/category_apex.jpg';
+import cyberpunk from '../../images/category_cyberpunk.jpg';
+import among from '../../images/category_among.jpg';
+import gtav from '../../images/category_gtav.jpg';
+import minecraft from '../../images/category_minecraft.jpg';
+import rdredemption from '../../images/category_rdredemption.jpg';
 
 export default [
   { name: 'League of Legends', source: lol },
@@ -12,4 +17,9 @@ export default [
   { name: 'Fortnite', source: fortnite },
   { name: 'Fall Guys', source: fallguys },
   { name: 'Apex Legends', source: apex },
+  { name: 'Cyberpunk 2077', source: cyberpunk },
+  { name: 'Among Us', source: among },
+  { name: 'Grand Theft Auto V', source: gtav },
+  { name: 'Minecraft', source: minecraft },
+  { name: 'Red Dead Redemption 2', source: rdredemption },
 ];
